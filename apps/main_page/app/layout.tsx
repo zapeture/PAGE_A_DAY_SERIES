6cc2857e2a325af832fcb7e3caa44648862c0182
Fortune Zviregei <fortunechainz@gmail.com>
2023-12-27 22:59:49 +0100

    Update global CSS and layout

--- apps/main_page/app/layout.tsx
+++ apps/main_page/app/layout.tsx
@@ -21,8 +21,9 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={russo.className}>{children}
-        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossOrigin="anonymous" />
+      <body className={`${russo.className} container-fluid`}>
+        <main className='min-vh-100'>{children}</main>
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"/>
       </body>
     </html>
   );
