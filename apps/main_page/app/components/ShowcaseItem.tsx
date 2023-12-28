607e80c297a165e59c799be488a312facdd3c8ed
Fortune Zviregei <fortunechainz@gmail.com>
2023-12-28 19:09:13 +0100

    Refactor ShowcaseItem component and update footer copyright opacity

--- apps/main_page/app/components/ShowcaseItem.tsx
+++ apps/main_page/app/components/ShowcaseItem.tsx
@@ -1,11 +1,17 @@
+"use client"
+import { motion } from "framer-motion"
+
 export default function ShowcaseItem(): JSX.Element {
-  return <div className="col-12 col-md-6 col-lg-4">
+  return <motion.a className="col-12 col-md-6 col-lg-4 text-decoration-none" href="https://google.com" target="_blank">
     <div className="card showcase-card">
-      <div className="image-wrapper card-image-wrapper">
-      </div>
+      <motion.div className="image-wrapper card-image-wrapper" whileHover={{ scale: 1.02, translateY: -10 }} transition={{
+        ease: "easeInOut",
+        duration: 0.3
+      }}>
+      </motion.div>
       <div className="card-body">
         <h5 className="card-title">An app</h5>
       </div>
     </div>
-  </div>;
+  </motion.a>;
 }
