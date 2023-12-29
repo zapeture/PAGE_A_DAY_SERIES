cc917d3ebd4f1cbdd7b8c6c47e897bf78c1554bc
Fortune Zviregei <fortunechainz@gmail.com>
2023-12-30 00:40:35 +0100

    Revalidate cache every 10 minutes

--- apps/main_page/queries/projects.ts
+++ apps/main_page/queries/projects.ts
@@ -4,7 +4,7 @@ import { sanityUrlBuilder } from "../helpers/url";
 export const getProjects = async (): Promise<Projects> => {
   try {
 
-    const res = await fetch(sanityUrlBuilder("project"));
+    const res = await fetch(sanityUrlBuilder("project"), { next: { revalidate: 600 } });
 
     if (!res.ok) {
       console.error("Fetching projects failed:", res.status);
