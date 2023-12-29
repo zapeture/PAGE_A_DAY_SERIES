0efc94797b2d5453b23c3a8e9c1d0c4e723ad461
Fortune Zviregei <fortunechainz@gmail.com>
2023-12-29 22:01:10 +0100

    Add image support to ShowcaseItem component

--- apps/main_page/app/components/ShowcaseItem.tsx
+++ apps/main_page/app/components/ShowcaseItem.tsx
@@ -1,14 +1,23 @@
 "use client"
+import Image from "next/image";
 import { motion } from "framer-motion"
 import { Project } from '../model/project';
+import { client } from "../../sanity/lib/client";
+import imageUrlBuilder from '@sanity/image-url'
 
-export default function ShowcaseItem({project}: {project: Project}): JSX.Element {
+export default function ShowcaseItem({ project }: { project: Project }): JSX.Element {
+  const builder = imageUrlBuilder(client)
   return <motion.a className="col-12 col-md-6 col-lg-4 text-decoration-none" href={project.url} target="_blank">
     <div className="card showcase-card">
       <motion.div className="image-wrapper card-image-wrapper" whileHover={{ scale: 1.02, translateY: -10 }} transition={{
         ease: "easeInOut",
         duration: 0.3
       }}>
+        {builder.image(project.thumbNail.asset).url() && <Image src={builder.image(project.thumbNail.asset).url()} alt={project.thumbNail.alt}
+          layout="fill"
+          sizes="100vw"
+          style={{ width: '100%' }}
+        />}
       </motion.div>
       <div className="card-body">
         <h5 className="card-title">{project.title}</h5>
--- apps/main_page/queries/projects.ts
+++ apps/main_page/queries/projects.ts
@@ -4,13 +4,13 @@ import { sanityUrlBuilder } from "../helpers/url";
 export const getProjects = async (): Promise<Projects> => {
   try {
 
-    console.log(sanityUrlBuilder("project"));
     const res = await fetch(sanityUrlBuilder("project"));
 
     if (!res.ok) {
       console.error("Fetching projects failed:", res.status);
     }
     const data = await res.json();
+
     return data.result as Projects;
   } catch (error) {
     console.error("Fetching projects failed:", error);
