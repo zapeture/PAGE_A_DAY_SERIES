d7ca0e874bc241335b3fc02c5648d4c3dad09ba0
Fortune Zviregei <fortunechainz@gmail.com>
2024-03-24 00:37:17 +0100

    Refactor ShowcaseGrid component to sort projects by status

--- apps/main_page/app/components/ShowcaseGrid.tsx
+++ apps/main_page/app/components/ShowcaseGrid.tsx
@@ -4,12 +4,31 @@ import ShowcaseItem from "./ShowcaseItem";
 
 export default async function ShowcaseGrid() {
   const projects = await getProjects() as Projects;
-  return <section className="showcase-grid">
-    {projects && projects.length > 0 && <div className="row row-gap-4">
-      {projects.map((project: Project) => {
-        return <ShowcaseItem project={project} key={project._id}/>;
-      })}
-    </div>}
 
-  </section>;
+  const sortedProjects = projects.sort((a: Project, b: Project) => {
+    if (a.status === "live" && b.status !== "live") {
+      return -1;
+    } else if (a.status !== "live" && b.status === "live") {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  const renderProjects = () => {
+    if (sortedProjects && sortedProjects.length > 0) {
+      return sortedProjects.map((project: Project) => (
+        <ShowcaseItem project={project} key={project._id} />
+      ));
+    }
+    return null;
+  };
+
+  return (
+    <section className="showcase-grid">
+      <div className="row row-gap-4">
+        {renderProjects()}
+      </div>
+    </section>
+  );
 }
