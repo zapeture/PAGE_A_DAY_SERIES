e4c7a721e8096ed48b7d181bd7a0db064831083d
Fortune Zviregei <fortunechainz@gmail.com>
2024-04-18 03:55:08 +0200

    Update Sandstone layout and styles

--- apps/sandstone/app/page.js
+++ apps/sandstone/app/page.js
@@ -1,6 +1,9 @@
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <h1>Subscribe to our Newsletter</h1>
+      <p>em ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices. Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p>
+    </div>
   );
 }
