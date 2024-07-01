66f34caee3ffd76abfda6a9c6820525072250113
Fortune Zviregei <fortunechainz@gmail.com>
2024-07-01 17:28:19 +0200

    chore: Add BelowTheFoldSection component and update global styles

--- apps/destiny/app/page.tsx
+++ apps/destiny/app/page.tsx
@@ -1,9 +1,11 @@
+import BelowTheFoldSection from "@/components/BelowTheFoldSection";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <BelowTheFoldSection />
     </>
   );
 }
