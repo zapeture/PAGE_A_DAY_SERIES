cb34b56ab68f04e717b87832fe0ddb243e70c049
Fortune Zviregei <fortunechainz@gmail.com>
2025-04-15 14:43:37 +0200

    Add Footer component to layout, replacing inline footer with a separate Footer component for improved structure and maintainability.

--- apps/main_page/app/layout.tsx
+++ apps/main_page/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Russo_One } from "next/font/google";
 import Script from "next/script";
+import Footer from "./components/Footer";
 
 const russo = Russo_One({
   weight: "400",
@@ -42,19 +43,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }): JSX.Element {
-  const currentYear = new Date().getFullYear();
   return (
     <html lang="en">
-      <head>
-        <link rel="apple-icon" sizes="180x180" href="/apple-icon.png" />
-      </head>
       <body className={`${russo.className} container-fluid`}>
         <main className="min-vh-100">{children}</main>
-        <footer className="footer d-flex py-4">
-          <span className="d-block mx-auto fs-7 opacity-50">
-            Copyright@Fortune Zviregei {currentYear}
-          </span>
-        </footer>
+        <Footer />
         <Script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
           async
